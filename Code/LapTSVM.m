7d1c01517149567a098400165c159ee123d229fc
aritraghosh <aritrag94@gmail.com>
2015-04-24 19:18:33 +0530

    Doesnt Work :(

--- Code/LapTSVM.m
+++ Code/LapTSVM.m
@@ -1,24 +1,26 @@
 load('2moons.mat');
 M = x;
-true_labels = y;
+true_labels = zeros(200,1);
+true_labels(123) = 1;
+true_labels(142) = -1;
 
 total = size(M,1);
 %1:positive
 %-1: negative
 %First manually make entries in dataset 0- unlabelled
 
-positive_indices = find(y==1);
+positive_indices = find(true_labels==1);
 A = M(positive_indices,:);
 
-negative_indices = find(y==-1);
+negative_indices = find(true_labels==-1);
 B = M(negative_indices,:);
 
 % Parameters
 c_1 = 4;
 c_2 = 0.024;
 c_3 = 1;
-sigma = 5.0;
-k = 10;
+sigma = 0.2;
+k =1;
 
 % Calculate L using D,W (First we need to find W)
 IDX = knnsearch(M,M,'K',k);
@@ -54,5 +56,9 @@ predicted = 2*(min(f_plus,f_minus) == f_plus)-1;
 correct = (predicted == y);
 fprintf('Accuracy: %f\n',sum(correct)/size(correct,1));
 
-
+figure;
+scatter(x(:,1),x(:,2),[],predicted);
+hold on;
+scatter(x(117,1),x(117,2),[],'r');
+scatter(x(118,1),x(118,2),[],'b');
 
--- Code/negativeLapSVM.m
+++ Code/negativeLapSVM.m
@@ -8,15 +8,15 @@ function [rho_minus] = negativeLapSVM(L,A,B,M,c_1,c_2,c_3,sigma)
     F_phi =horzcat(computeRBFKernel(M,M,sigma), e);
     e_plus = ones(n_positive,1); %m1
     e_minus = ones(n_negative,1); % m2
-    Q_phi = horzcat(computeRBFKernel(B,M,sigma) ,e_minus);
-    P_phi =  horzcat(computeRBFKernel(A,M,sigma),e_plus);
+    Q_phi = horzcat(computeRBFKernel(A,M,sigma) ,e_plus);
+    P_phi =  horzcat(computeRBFKernel(B,M,sigma),e_minus);
 
-    H = -1*P_phi* inv(Q_phi'*Q_phi + c_2*U_phi+ c_3*F_phi'*L*F_phi)*P_phi';
+    H = P_phi* inv(Q_phi'*Q_phi + c_2*U_phi+ c_3*F_phi'*L*F_phi)*P_phi';
 
     lb= zeros(n_positive,1); % lower bound for alpha
     ub = c_2* e_plus; % upper bound for beta
-    f = e_plus ;
-    beta = quadprog(-H,-f,[],[],[],[],lb,ub);
+    f = -e_plus ;
+    beta = quadprog(H,f,[],[],[],[],lb,ub);
 
     %rho_plus = [lamda_minus b_minus]^T
     % Is minus symbol required?
